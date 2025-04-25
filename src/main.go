087b67d26c864b21f3a7daf30fd49af60807c86e
Eswar Vallabha <ramagownieswar@karunya.edu.in>
2025-04-25 15:53:58 +0530

    Updated some changes

--- src/main.go
+++ src/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"main/src/modules/job"
+	"net/http"
+	"os"
 )
 
 func main() {
 	errors := []string{`Client.Timeout exceeded while awaiting header`,
 		"invalid session. Kindly logout and login again",
 	}
-
-	// job.Start(errors, false)
 
 	job.Start(errors, false)
 
